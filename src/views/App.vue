6a4a0c6e37281c445b31da91759ea07f29c8fa34
Wifsimster <wifsimster@gmail.com>
2017-06-10 12:45:30 +0200

    Add logo

--- src/views/App.vue
+++ src/views/App.vue
@@ -1,12 +1,21 @@
 <template>
-<div class="app"> 
+<div class="app">
+    <img :src="logo">
     <router-view></router-view>
     </div>
 </template>
 
 <script>
+    import logo from '../assets/logo.jpg'
     export default {
+        data() {
+            return {
+                logo: logo,
+            }
+        }
     }
 </script>
 
-<style lang="sass"></style>
+<style lang="sass">
+@import '../sass/global';
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "build.js",
-        publicPath: "/dist/",
+        publicPath: "dist",
     },
     module: {
         rules: [
