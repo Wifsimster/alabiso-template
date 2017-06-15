a4f6f8828a3336f49620d297322daf5577fb0901
Wifsimster <wifsimster@gmail.com>
2017-06-15 22:46:43 +0200

    :sun_with_face:

--- src/components/ContactMap.vue
+++ src/components/ContactMap.vue
@@ -1,11 +1,23 @@
 <template>
-<div class="map">
-    </div>
+<div class="map" id="map"></div>
+
+<!-- AIzaSyBMJguOJhxaNLbsLAwB57fhWHKaIKR-KW4 -->
+
+<!--43.6171734,3.822794,17z-->
+
 </template>
 
 <script>
     export default {
-        
+        created() {
+            this.$nextTick(() => {
+                new GMaps({
+                    div: '#map',
+                    lat: -12.043333,
+                    lng: -77.028333
+                });
+            })
+        },
     }
 </script>
 
--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -2,38 +2,105 @@
 <div>
     <div class="swiper-container">
         <div class="swiper-wrapper">
-            <div class="swiper-slide">Référence 1</div>
-            <div class="swiper-slide">Référence 2</div>
-            <div class="swiper-slide">Référence 3</div>
+            <div class="swiper-slide">
+                <card>
+                    <div slot="image"><img v-lazy="kohinoor"></div>
+                    <span slot="title">Koh I Noor</span>
+                    <span slot="description">49 Logements à Port Marianne, Rive Gauche à Montpellier. 
+                        1er Prix Pyramide d'Argent</span>
+                    <span slot="reveal-title">Koh I Noor</span>
+                    <span slot="reveal-description">
+                        <strong>Maître d'Ouvrage:</strong>
+                        <p>Kalelithos - SCI Belle Pierre</p>
+                        <strong>Architecte:</strong>
+                        <p>Bernard BUHLER ARCHITECTE</p>
+                        <strong>Mission:</strong>
+                        <p>Chauffage - Ventilation - Plomberie - ECS - Electricité</p>
+                        <strong>Montant des travaux:</strong>
+                        <p>5M € HT</p>
+                        <strong>Année:</strong>
+                        <p>En cours</p></span></card>
     </div>
-        <div class="swiper-pagination"></div>
+            <div class="swiper-slide">
+                <card>
+                    <div slot="image"><img v-lazy="terraVerde"></div>
+                    <span slot="title">Le Terra Verde</span>
+                    <span slot="description">Bureaux 4000m², Park Eurêka à Montpellier. 
+                        Label BREEAM</span>
+                    <span slot="reveal-title">Le Red Line</span>
+                    <span slot="reveal-description">
+                        <strong>Maître d'Ouvrage:</strong>
+                        <p>Groupe DENUX – SM SERVICE</p>
+                        <strong>Architecte:</strong>
+                        <p>Gilles CHRETIEN </p>
+                        <strong>Mission:</strong>
+                        <p>Chauffage - Climatisation - Ventilation - Plomberie - ECS - Electricité - Commissionning</p>
+                        <strong>Montant des travaux:</strong>
+                        <p>6.5M € HT</p>
+                        <strong>Année:</strong>
+                        <p>En cours</p></span></card>
+    </div>
+            <div class="swiper-slide">
+                <card>
+                    <div slot="image"><img v-lazy="redline"></div>
+                    <span slot="title">Le Red Line</span>
+                    <span slot="description">Bureaux de 2200m² à Castelnau le Lez.</span>
+                    <span slot="reveal-title">Le Red Line</span>
+                    <span slot="reveal-description">
+                        <strong>Maître d'Ouvrage:</strong>
+                        <p>Angelotti & SCCV Red Line</p>
+                        <strong>Architecte:</strong>
+                        <p>Philippe RUBIO Architectes</p>
+                        <strong>Mission:</strong>
+                        <p>Chauffage - Climatisation - Ventilation - Plomberie - ECS - Electricité</p>
+                        <strong>Montant des travaux:</strong>
+                        <p>-</p>
+                        <strong>Année:</strong>
+                        <p>2012</p></span></card>
+    </div>
+    </div>
+        <!--        <div class="swiper-pagination"></div>-->
         <div class="swiper-button-prev"></div>
         <div class="swiper-button-next"></div>
-        <div class="swiper-scrollbar"></div>
+        <!--        <div class="swiper-scrollbar"></div>-->
     </div>
     </div>
 </template>
 
 <script>
-    import 'swiper/dist/js/swiper.min.js'
     import 'swiper/dist/css/swiper.min.css'
+    import 'swiper/dist/js/swiper.min.js'
+    import Card from '../components/Card.vue'
+    import kohinoor from '../assets/kohinoor.jpg'
+    import terraVerde from '../assets/terra_verde.jpg'
+    import redline from '../assets/redline.jpg'
     export default {
         name: 'Slider',
-        methods: {},
+        components: { Card },
+        data() {
+            return {
+                swiper: null,
+                kohinoor: kohinoor,
+                terraVerde: terraVerde,
+                redline: redline,
+            }
+        },
         created() {
-            const mySwiper = new Swiper ('.swiper-container', {
-                autoplay: 2000,
-                speed: 400,
-                spaceBetween: 100,
-                watchSlidesProgress: true,
-                direction: 'vertical',
-                loop: true,
-                pagination: '.swiper-pagination',
-                nextButton: '.swiper-button-next',
-                prevButton: '.swiper-button-prev',
-                scrollbar: '.swiper-scrollbar',
-            })   
-            },
+            this.$nextTick(() => {
+                this.swiper = new Swiper('.swiper-container', {
+                    autoplay: 2000,
+                    speed: 400,
+                    spaceBetween: 100,
+                    watchSlidesProgress: true,
+                    direction: 'horizontal',
+                    loop: true,
+                    //                    pagination: '.swiper-pagination',
+                    nextButton: '.swiper-button-next',
+                    prevButton: '.swiper-button-prev',
+                    //                    scrollbar: '.swiper-scrollbar',
+                })
+            })
+        },
     }
 </script>
 
