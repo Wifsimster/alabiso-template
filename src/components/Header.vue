4a57b2983877877aa62d8051ae9e430aa0606767
Wifsimster <wifsimster@gmail.com>
2017-09-15 17:37:54 +0200

    Add css grid to page template

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="header">
+        <img v-lazy="logo" class="logo pure-img">
+    </div>
+</template>
+
+<script>
+import logo from '../assets/logo.jpg'
+export default {
+    data() {
+        return {
+            logo: logo,
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+@import '../sass/components/header'
+</style>
--- src/views/App.vue
+++ src/views/App.vue
@@ -1,30 +1,29 @@
 <template>
-<div class="app">
-    <div class="wrapper">        
-        <img v-lazy="logo" class="logo pure-img">
+    <div class="container">
+        <!-- <div class="wrapper"> -->
+        <com-header></com-header>
         <tabs></tabs>
         <div class="content">
             <router-view></router-view>
-    </div>
-    </div>
-    <com-footer></com-footer>
+        </div>
+        <!-- </div> -->
+        <com-footer></com-footer>
     </div>
 </template>
 
 <script>
-    import logo from '../assets/logo.jpg'
-    import Tabs from '../components/Tabs.vue'
-    import ComFooter from '../components/Footer.vue'
-    export default {
-        components: { Tabs, ComFooter },
-        data() {
-            return {
-                logo: logo,
-            }
-        }
-    }
+import ComHeader from '../components/Header.vue'
+import Tabs from '../components/Tabs.vue'
+import ComFooter from '../components/Footer.vue'
+export default {
+    components: {
+        ComHeader,
+        Tabs,
+        ComFooter
+    },
+}
 </script>
 
-<style lang="sass">
+<style lang="scss">
 @import '../sass/global'
 </style>
